82750f73dd0e0182b695d8556ce6118fa64cc940
Yucan Yu (Student) <yucanyu@sundevils.asu.edu>
2023-05-01 23:46:14 -0700

    add website files

---
+++ quiz.js
@@ -0,0 +1,131 @@
+let quizData = [];
+let dynamicTextData = [];
+let currentQuestion = 0;
+let preQuizNum = 0;
+let preQuizScore = 0.0;
+let postQuizScore = 0.0;
+let preTotalGrade = 0;
+let postTotalGrade = 0;
+const data = `Do you think gender stereotype truely exist?|No|Yes
+Degrees is know to be a determining factor to job. Which sex do you think earn more degree?|Man|Female
+Which of the following careers has been perceived as more suitable for men? (Choose one)|Nursing|Teaching|Fashion Designing|Engineering
+Which of the following careers has been perceived as more suitable for women? (Choose one)|Construction|Police/cop|Software Development|Nursing
+Which of the following carrers has been perceived as more suitable for transwoman?|Soldier|Elementary School Teacher|Nursing|Unknown (or, depends on the specific person)
+Which of the following carrers has been perceived as more suitable for transman?|Soldier|Elementary School Teacher|Nursing|Unknown (or, depends on the specific person)
+Which of the following carrers has been perceived as more suitable for non-biarny/gender-queer(gender non-conforming people)?|Soldier|Elementary School Teacher|Nursing|Unknown (or, depends on the specific person)
+Do transgender people(people who do not present themselves as being fitting into the traditional binary genders) still face employment discrimination?|No|Yes
+What is the primary reason behind your career choice?|Personal Interest|Societal Expectations|Family Pressure|Financial Stability
+Do you believe gender stereotypes have influenced your career choice?|I am not sure|No|Maybe|Yes
+
+Case A: Unfortunately, your knowledge on gender stereotype and its influence on career choices are quite poor. It is a fact that gender stereotype truly exists as well as their influence on career choices. For example, the sex-based disparity in the fields of science, technology, engineering, and mathematics (STEM) continues to exist in the United States to this date (Lin). Females actually earn 57% of all degrees last year. Engineering has been perceived as more suitable for men since the male population overwhelmingly outweighs the female population in this industry. Nursing has been perceived as more suitable for men since the female population overwhelmingly outweighs the male population in this industry. For transmen, transwomen, and gender non-conforming people, there may not be the most suitable career. It is quite hard to put them into boxes since they are all extremely diverse. Furthermore, transgender people still face employment discrimination."According to a study by the National Center for Transgender Equality (NTDS), 67% out of 70% of participants who reported having a job or having applied to a job in the last year were fired, not hired, or not promoted" (Abreu). Everyone has different reasons for their own career choices since we all have the right to live according to our own will. Gender stereotypes do objectively affect people's career choices, but each person may feel differently about it. I will share a personal story with you here. The story was told by my grandma about one of her friends. My grandma was born in a wealthy landowner's house in the 195s in China, and she is well-known for her independence and compliance with the cultural norm. After the cultural revolution started when she was 20, the family's house was taken away. She tried to look for a job in a steel factory, but she is deemed unsuitable as a woman who is actually capable of doing the so-called "man's" job at that time. It took a long time for my grandma's friend to find a job. If society had less discrimination, then life would be easier for her. I design this project to encourage everyone to alleviate their own prejudice and practice equity in the workplace.
+Case B: I'm glad you learned something new today! You have had a substantial amount of knowledge of gender stereotype and their influence on career choices. It is a fact that gender stereotype truly exists as well as their influence on career choices. For example, the sex-based disparity in the fields of science, technology, engineering, and mathematics (STEM) continues to exist in the United States to this date (Lin). Females actually earn 57% of all degrees last year. Engineering has been perceived as more suitable for men since the male population overwhelmingly outweighs the female population in this industry. Nursing has been perceived as more suitable for men since the female population overwhelmingly outweighs the male population in this industry. For transmen, transwomen, and gender non-conforming people, there may not be the most suitable career. It is quite hard to put them into boxes since they are all extremely diverse. Furthermore, transgender people still face employment discrimination."According to a study by the National Center for Transgender Equality (NTDS), 67% out of 70% of participants who reported having a job or having applied to a job in the last year were fired, not hired, or not promoted" (Abreu). Gender stereotypes do objectively affect people's career choices, but each person may feel differently about it. I will share a personal story with you here. My friend as quite a feminine woman earned her degree in engineering from USC, but she was rejected for a couple of jobs in this industry because she was deemed unfitting. Meanwhile, she graduate summa cum laude as a graduate engineering student and was respected for her hard work and intelligence in the school. I think gender stereotypes stop her from succeeding in her career which I believe is unfair. I design this project to encourage everyone to alleviate their own prejudice and practice equity in the workplace.
+Case C: Pretty good! You're on the right track! You have had a substanial amount of knowledge on gender stereotype and its influence on career choices. It is a fact that gender stereotype truly exists as well as their influence on career choices. For example, the sex-based disparity in the fields of science, technology, engineering, and mathematics (STEM) continues to exist in the United States to this date (Lin). Females actually earn 57% of all degrees last year. I will skip the most suitable job for men and the most suitable job for women since you already excel on this quiz. For transmen, transwomen, and gender non-conforming people, there may not be the most suitable career. It is quite hard to put them into boxes since they are all extremely diverse. Furthermore, transgender people still face employment discrimination."According to a study by the National Center for Transgender Equality (NTDS), 67% out of 70% of participants who reported having a job or having applied to a job in the last year were fired, not hired, or not promoted" (Abreu). Everyone has different reasons for their own career choices since we all have the right to live our own life according to our own will. Gender stereotypes do objectively affect people's career choices, but each person may feel differently about it. I will share a personal story with you here. My aunt was a woman who works in the stem industry. She was a very hard-working student when she was at Tsinghua University. She later attended UC Berkeley for her Computer Science Master's degree and eventually emigrated to the United States. At the time, Scilicon Valley was growing and she tried to seek a job in Scilicon Valley. She dreamed of becoming Larry Ellison, Bill Gates, and Steven Jobs. She founded her own company, but the company was rejected to be invested after seeing my aunt was a woman. Silicon Valley was emersed in a hard-working and non-stopping culture. Till now, she became a housewife and given up her dream. I think gender stereotypes stop her from succeeding in her career which I believe is unfair. I design this project to encourage everyone to alleviate their own prejudice and practice equity in the workplace.
+Case D: Excellent! You master most of knowledge on gender stereotype and its influence on career choices. It is a fact that gender stereotype truly exists as well as their influence on career choices. For example, the sex-based disparity in the fields of science, technology, engineering, and mathematics (STEM) continues to exist in the United States to this date (Lin). I will skip the rate of getting a Bachelor's degree, the most suitable job for men, and the most suitable job for women since you already excel on this quiz. For transmen, transwomen, and gender non-conforming people, there may not be the most suitable career. It is quite hard to put them into boxes since they are all extremely diverse. Furthermore, transgender people still face employment discrimination. "According to a study by the National Center for Transgender Equality (NTDS), 67% out of 70% of participants who reported having a job or having applied to a job in the last year were fired, not hired, or not promoted" (Abreu). I will share a personal story with you here. There was a funny story in my family. My father's friend's son was interested in becoming a doctor eventually, but he is required to become a nurse first. After finishing his medical degree, he was rejected to become a nurse because he is a tall and fit guy. Though he is hired now, he is known for his good attitude and patience in the workplace. Prior to his employment, he struggled with being hired to be a nurse for a long time because of his gender identity and appearance. I think gender stereotypes stop him from succeeding in her career which I believe is unfair. I design this project to encourage everyone to alleviate their own prejudice and practice equity in the workplace.
+
+After learning about the impact of gender stereotypes on career choices, has your opinion on the influence of gender stereotypes on career choices changed?|I am not sure|No|Maybe|Yes
+Are you more likely to challenge gender stereotypes in your career planning and choices now?|I am not sure|No|Maybe|Yes
+Do you feel more aware of the negative consequences of gender stereotypes on career choices and professional growth after the dynamic education section?|I am not sure|No|Maybe|Yes
+How important is it to promote diversity and inclusion in the workplace?|Not important|Somewhat important|Neutral|Extremely important
+
+Unfortunately this test doesn't seem to be that helpful. If you want to know more, please google related content. A better future starts with you and me!
+Thank you very much for taking part in this test. Hope it helps you. We encounter more prejudices in real life, and we should have the courage to challenge these stereotypes. If you want to know more, please google related content. A better future starts with you and me!
+It is good to know that your gender bias decreased after passing this test. I hope you can recall this knowledge in the actual workplace and avoid these deviations. Have a nice day!
+`;
+
+const [preQuizContent, dynamicTextContent, postQuizContent, feedbackTextContent] = data.split("\n\n");
+if (!preQuizContent || !dynamicTextContent || !postQuizContent || !feedbackTextContent) {
+    console.error("The data string does not have the expected format");
+} else {
+    quizData = preQuizContent.split("\n").map(line => line.split("|"));
+    preQuizNum = quizData.length;
+    dynamicTextData = dynamicTextContent.split("\n").map(line => line.split("|"));
+    quizData = quizData.concat(postQuizContent.split("\n").map(line => line.split("|")));
+    feedbackTextData = feedbackTextContent.split("\n").map(line => line.split("|"));
+    displayQuestion();
+}
+
+function displayQuestion() {
+    const quizContainer = document.getElementById("quiz-container");
+    const answerForm = document.getElementById("answer-form");
+    quizContainer.innerHTML = quizData[currentQuestion][0];
+    while (answerForm.firstChild) {
+        answerForm.removeChild(answerForm.firstChild);
+    }
+    for (let i = 1; i < quizData[currentQuestion].length; i++) {
+        const optionLabel = document.createElement("label");
+        const optionInput = document.createElement("input");
+        optionInput.type = "radio";
+        optionInput.name = "option";
+        optionInput.value = i - 1;
+        optionLabel.appendChild(optionInput);
+        optionLabel.appendChild(document.createTextNode(quizData[currentQuestion][i]));
+        answerForm.appendChild(optionLabel);
+        answerForm.appendChild(document.createElement("br"));
+    }
+}
+
+
+function nextQuestion() {
+    const selectedOption = document.querySelector("input[name=option]:checked");
+    if (!selectedOption) return;
+    const answer = selectedOption.value;
+    if (currentQuestion < preQuizNum) {
+        preTotalGrade += quizData[currentQuestion].length-1;
+        preQuizScore += parseInt(answer)*(quizData[currentQuestion].length-1)/(quizData[currentQuestion].length-2);
+    } else {
+        postTotalGrade += quizData[currentQuestion].length-1;
+        postQuizScore += parseInt(answer)*(quizData[currentQuestion].length-1)/(quizData[currentQuestion].length-2);
+    }
+    selectedOption.checked = false;
+    currentQuestion++;
+    if (currentQuestion === preQuizNum) {
+        displayDynamicText(preQuizScore);
+    } else if (currentQuestion === quizData.length) {
+        displayFeedback(postQuizScore);
+    } else {
+        displayQuestion();
+    }
+}
+
+function displayDynamicText(score) {
+    document.getElementById("quiz-container").style.display = "none";
+    document.getElementById("answer-form").style.display = "none";
+    document.getElementById("next-btn").style.display = "none";
+    const dynamicTextContainer = document.getElementById("dynamic-text-container");
+    dynamicTextContainer.style.display = "block";
+    const dynamicText = document.getElementById("dynamic-text");
+    dynamicText.textContent = "Thank you!"
+    for (let i = 1; i <= dynamicTextData.length; i++) {
+        if (score <= preTotalGrade * i / dynamicTextData.length) {
+            dynamicText.textContent = dynamicTextData[i-1].join(" ");
+            break;
+        }
+    }
+}
+
+function displayFeedback(score) {
+    document.getElementById("quiz-container").style.display = "none";
+    document.getElementById("answer-form").style.display = "none";
+    document.getElementById("next-btn").style.display = "none";
+    document.getElementById("continue-btn").style.display = "none";
+    const dynamicTextContainer = document.getElementById("dynamic-text-container");
+    dynamicTextContainer.style.display = "block";
+    const dynamicText = document.getElementById("dynamic-text");
+    dynamicText.textContent = "Thank you!"
+    for (let i = 1; i <= feedbackTextData.length; i++) {
+        if (score < postTotalGrade * i / feedbackTextData.length) {
+            console.log(i)
+            console.log(feedbackTextData[i-1])
+            dynamicText.textContent = feedbackTextData[i-1].join(" ");
+            break;
+        }
+    }
+}
+
+function continueQuiz() {
+    document.getElementById("dynamic-text-container").style.display = "none";
+    document.getElementById("quiz-container").style.display = "block";
+    document.getElementById("answer-form").style.display = "block";
+    document.getElementById("next-btn").style.display = "block";
+    displayQuestion();
+}
